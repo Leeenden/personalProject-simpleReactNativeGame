6ea3aa43469eb1abe08c89ef84cd689de201adb2
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-08-23 17:59:54 +0100

    installed redux and added the neccessary files

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
+import React from "react";
 import Gameboard from './components/Gameboard';
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 export default function App() {
   return (
-    <Gameboard />
+    <Provider store={store}>
+      <Gameboard />
+    </Provider>
+    
   )
 }
 
